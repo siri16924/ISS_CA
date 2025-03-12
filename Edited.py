2a9e9b8c4f5a6fbf98232ce103385ec28a6737a2
siri16924 <kattasreechandana@gmail.com>
2025-03-12 09:56:56 +0530

    Create Edited.py

---
+++ Edited.py
@@ -0,0 +1,14 @@
+def is_narc(n):
+    num_str = str(n)
+    num_digits = len(num_str)
+    
+    sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
+    
+    return sum_of_powers == n
+
+def print_narcis_numbers(start, end):
+    for num in range(start, end + 1):
+        if is_narc(num):
+            print(num)
+
+print_narcis_numbers(1000, 5000)
